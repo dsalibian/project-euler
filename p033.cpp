14f3e9ab1a9349a165f935198517ba7bee75cad5
dantesall <dante.salibian@gmail.com>
2024-05-24 20:33:23 -0700

    good

---
+++ p033.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+using namespace std;
+
+bool g(int a, int b) {
+    if( a % 10 == 0 && b % 10 == 0 )
+        return false;
+
+    double r = (double)a / b;
+
+    double p, q;
+
+    int a_0 = a % 10;
+    int a_1 = ( a / 10 ) % 10;
+
+    int b_0 = b % 10;
+    int b_1 = ( b / 10 ) % 10;
+
+    if( a_0 == b_0 ) {
+        p = a_1;
+        q = b_1;
+    }
+    else if( a_0 == b_1 ) {
+        p = a_1;
+        q = b_0;
+    }
+    else if( a_1 == b_0 ) {
+        p = a_0;
+        q = b_1;
+    }
+    else if( a_1 == b_1 ) {
+        p = a_0;
+        q = b_0;
+    }
+    else    
+        return false;
+
+    return p / q == r;
+
+}
+
+int f(int n) {
+    int a = 1;
+    int b = 1;
+
+    for(int i = 10; i < 100; i++)
+        for(int j = i + 1; j < 100; j++)
+            if( g(i, j) ) {
+                a *= i;
+                b *= j;
+            }
+    
+    for( ; !( a & 1 ) && !( b & 1 ) ; ) {
+        a /= 2;
+        b /= 2;
+    }
+
+    for(int i = 3; i*i <= max(a, b); i += 2) {
+        for( ; ( a % i == 0 ) && ( b % i == 0 ); ) {
+            a /= i;
+            b /= i;
+        }
+    }
+
+    return b;
+}
+
+int main() {
+    int n = 2;
+
+    auto start = chrono::steady_clock::now();
+    int result = f(n);
+    auto end = chrono::steady_clock::now();
+    
+    cout << result << endl;
+    cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+
+    return 0;
+}
