165f7199bb1e2bc328e231e30e790b66cea4b4a6
dantesall <dante.salibian@gmail.com>
2024-05-29 18:40:44 -0700

    good

---
+++ p587.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+#include <cmath>
+
+using namespace std;
+
+double F(double x) {
+    return -0.5*((x - 1)*sqrt(x*(2 - x)) - 2*x - asin(1 - x));
+}
+
+double di(double a, double b){
+    return F(b) - F(a);
+}
+
+double x(double n) {
+    return ( ( (n + 1) / n ) - sqrt(2.0/n) ) * ( (n*n) / (n*n + 1) );
+}
+
+int f(double n) {
+    double v_0 = di(0, 1);
+
+    for(int i = 1; ; i++) {
+        double a = x(i);
+        double v = di(a, 1) + 0.5*a*a*(1.0/i);
+
+        if( v/v_0 < n )
+            return i;
+    }
+}
+
+int main() {
+    double n = 0.001;
+
+    auto start = chrono::steady_clock::now();
+    int result = f(n);
+    auto end = chrono::steady_clock::now();
+    
+    cout << result << endl;
+    cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;
+
+    return 0;
+}
